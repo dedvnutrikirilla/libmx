6f23e0bc6ca62768f4c00b287ac2b115f3fbd145
Kyrylo <kandriushc@stud.khpi.ucode-connect.study>
2021-10-28 15:14:55 +0300

    libmx???

---
+++ src/mx_realloc.c
@@ -0,0 +1,27 @@
+#include "../inc/libmx.h"
+
+void *mx_realloc(void *ptr, size_t size) {
+    if (size <= 0) {
+        free(ptr);
+        ptr = NULL;
+        return NULL;
+    }
+
+    if (!ptr) {
+        return malloc(size);
+    }
+
+    size_t len = malloc_size(ptr);
+
+    if (len > size) {
+        size = len;
+    }
+
+    void *new_ptr = (void *)malloc(size);
+    new_ptr = mx_memmove(new_ptr, ptr, size);
+
+    free(ptr);
+    ptr = NULL;
+
+    return new_ptr;
+}
